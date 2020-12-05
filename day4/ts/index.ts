2b9ec56d61151d11b3bca68b52e77fb5286d3e16
Sandu Victor <vyctor661@gmail.com>
2020-12-05 15:50:23 +0200

    formatted ts versions with prettier

--- day4/ts/index.ts
+++ day4/ts/index.ts
@@ -35,48 +35,66 @@ const part1 = (input: Array<Array<String>>, codes: Array<string>): Number => {
   return s;
 };
 
-const hexRegex = new RegExp('#[0-9a-f]{6}')
-const eyeColors = new RegExp("amb|blu|brn|gry|grn|hzl|oth")
+const hexRegex = new RegExp("#[0-9a-f]{6}");
+const eyeColors = new RegExp("amb|blu|brn|gry|grn|hzl|oth");
 
 const part2 = (input: Array<Array<String>>, codes: Array<string>): Number => {
-    let s = 1; // for some reason im off by 1 and im starting this with 1 instead of 0
-    let z = 0;
-    input.map((v) => {
-      z = 0;
-      v.map((k) => {
-        for (let code of codes) if (k.startsWith(code)){
-            if(code == "byr"){
-                if (parseInt(k.split(':')[1]) >= 1920 && parseInt(k.split(':')[1]) <= 2002) z++
-            }
-            if(code == "iyr"){
-                if (parseInt(k.split(':')[1]) >= 2010 && parseInt(k.split(':')[1]) <= 2020) z++
-            }
-            if(code == "eyr"){
-                if (parseInt(k.split(':')[1]) >= 2020 && parseInt(k.split(':')[1]) <= 2030) z++
-            }
-            if(code == "hgt"){
-                let type = k.split(":")[1].split("")
-                let type2 = type[type.length-2] + type[type.length-1]
-                let value = parseInt(k.split(":")[1].split(type2)[0])
-                
-                if(type2 === 'cm') if(value >= 150 && value <= 193) z++
-                if(type2 === 'in') if(value >= 59 && value <= 76) z++
-            }
-            if(code == "hcl"){
-                if(hexRegex.test(k.split(':')[1])) z++
-            }
-            if(code == "ecl"){
-                if(eyeColors.test(k.split(":")[1])) z++;
-            }
-            if(code == "pid"){
-                if(k.split(":")[1].length === 9 && parseInt(k.split(":")[1]) >= 0 && parseInt(k.split(":")[1]) <= 999999999) z++;
-            }
+  let s = 1; // for some reason im off by 1 and im starting this with 1 instead of 0
+  let z = 0;
+  input.map((v) => {
+    z = 0;
+    v.map((k) => {
+      for (let code of codes)
+        if (k.startsWith(code)) {
+          if (code == "byr") {
+            if (
+              parseInt(k.split(":")[1]) >= 1920 &&
+              parseInt(k.split(":")[1]) <= 2002
+            )
+              z++;
+          }
+          if (code == "iyr") {
+            if (
+              parseInt(k.split(":")[1]) >= 2010 &&
+              parseInt(k.split(":")[1]) <= 2020
+            )
+              z++;
+          }
+          if (code == "eyr") {
+            if (
+              parseInt(k.split(":")[1]) >= 2020 &&
+              parseInt(k.split(":")[1]) <= 2030
+            )
+              z++;
+          }
+          if (code == "hgt") {
+            let type = k.split(":")[1].split("");
+            let type2 = type[type.length - 2] + type[type.length - 1];
+            let value = parseInt(k.split(":")[1].split(type2)[0]);
+
+            if (type2 === "cm") if (value >= 150 && value <= 193) z++;
+            if (type2 === "in") if (value >= 59 && value <= 76) z++;
+          }
+          if (code == "hcl") {
+            if (hexRegex.test(k.split(":")[1])) z++;
+          }
+          if (code == "ecl") {
+            if (eyeColors.test(k.split(":")[1])) z++;
+          }
+          if (code == "pid") {
+            if (
+              k.split(":")[1].length === 9 &&
+              parseInt(k.split(":")[1]) >= 0 &&
+              parseInt(k.split(":")[1]) <= 999999999
+            )
+              z++;
+          }
         }
-      });
-      if (z == 7) s++;
     });
-    return s;
-  };
+    if (z == 7) s++;
+  });
+  return s;
+};
 // console.log(format2(format));
 
 console.log(part1(format2(format), codes));

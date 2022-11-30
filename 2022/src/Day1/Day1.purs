0897487e9131784dcb1322e4f681cb9b8e781954
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-11-30 21:30:32 +0200

    preparing for AOC 2022
    set up the purescript project where I will give my solutions for AOC 2022, hopefully I get past day 6 this time lol, unfortunately in reality i have to deal with univesrity so i might not have time for AOC this year

---
+++ 2022/src/Day1/Day1.purs
@@ -0,0 +1,12 @@
+module Day1
+  ( main
+  )
+  where
+
+import Prelude
+
+import Effect.Console (log)
+
+
+main = do
+  log "hello"
---
+++ 2022/src/Main.purs
@@ -0,0 +1,11 @@
+module Main where
+
+import Prelude
+
+import Day1 as Current
+
+import Effect (Effect)
+
+main :: Effect Unit
+main = do
+  Current.main

658ba323facc4e19a60cfb4faf0f63cf70b2c14f
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-12-01 11:11:56 +0200

    day 1 done

--- 2022/spago.dhall
+++ 2022/spago.dhall
@@ -11,7 +11,18 @@ When creating a new Spago project, you can use
 to generate this file without the comments in this block.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "prelude" ]
+, dependencies =
+  [ "arrays"
+  , "console"
+  , "effect"
+  , "foldable-traversable"
+  , "integers"
+  , "maybe"
+  , "node-buffer"
+  , "node-fs"
+  , "prelude"
+  , "strings"
+  ]
 , packages = ./packages.dhall
-, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+, sources = [ "src/**/*.purs" ]
 }
--- 2022/src/Day1/Day1.purs
+++ 2022/src/Day1/Day1.purs
@@ -5,10 +5,35 @@ module Day1
 
 import Prelude
 
+import Data.Array (drop, length, sort, takeEnd, takeWhile, (:))
+import Data.Foldable (maximum, sum)
+import Data.Int (fromString)
+import Data.Maybe (fromMaybe)
+import Data.String (Pattern(..), split)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
 
+input :: Effect String
+input = readTextFile UTF8 "src/Day1/input.txt"
+ 
+parse :: String -> (Array (Int))
+parse s = (\a -> fromMaybe 0 a) <$> ((\a -> fromString a) <$> (split (Pattern "\r\n") s))
+
+format :: Array Int -> Array (Array Int)
+format arr = sadness arr []
+  where
+    sadness :: Array Int -> Array (Array Int) -> Array (Array Int)
+    sadness [] new = new
+    sadness old new = sadness (drop (length taken + 1) old) (taken : new)
+      where taken = (takeWhile (_ /= 0) old) 
 
 main ∷ Effect Unit
 main = do
-  log "hello"
+  content <- input
+  let finalArray = sum <$> (format $ parse content)
+  -- solution 1
+  logShow $ maximum finalArray
+  -- solution 2
+  logShow $ sum $ takeEnd 3 (sort finalArray) 
